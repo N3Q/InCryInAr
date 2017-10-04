2be46e310c77137b4571cc70f00d5d415fd4d291
N3Q <karstenfix@gmx.net>
2017-10-04 18:19:08 +0200

    SRC/IMP> ADD Main; DOC/QA> ADD INIT DOC; ROOT/QA> ADD gitignore

---
+++ src/de/fix/Main.java
@@ -0,0 +1,17 @@
+package de.fix;
+
+/**
+ * This Class is the Main Entry Point for the application.
+ *
+ * @author Karsten Fix, 04.10.17
+ */
+public class Main {
+
+    /**
+     * This Method is called, when the jar is executed.
+     * @param args The Command Line Arguments, that are passed.
+     */
+    public static void main(String[] args) {
+        // Todo: Implement.
+    }
+}
